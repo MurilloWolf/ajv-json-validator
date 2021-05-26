64a02264772f75c381274bab3ce37e849a20801c
MurilloWolf <mwolfcavalheiro@gmail.com>
2021-05-26 06:22:52 -0300

    Validaçao de um schema simples - Criar Projeto

---
+++ project.js
@@ -0,0 +1,37 @@
+import Ajv from "ajv"
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+
+const projectSchema = { 
+    type: "object",
+    properties: {
+        owner: {type: "string"},
+        title: {type: "string"},
+        description: {type: "string"},
+        createdAtt: {type: "string"},
+        updatedAtt:{type: "string"},
+    },
+    required: ["owner", "title", "description", "createdAtt"],
+    additionalProperties: false
+}
+/* 
+const schema = {
+  type: "object",
+  properties: {
+    foo: {type: "integer"},
+    bar: {type: "string"}
+  },
+  required: ["foo"],
+  additionalProperties: false
+} */
+
+const validate = ajv.compile(projectSchema)
+
+const data = {
+    owner: "Wolf",
+    title:"Diagrama 1",
+    description:"Primeiro diagrama do projeto",
+    createdAtt:"Wed May 26 2021 06:21:07 GMT-0300 (Horário Padrão de Brasília)",
+}
+
+const valid = validate(data)
+valid ? console.log('Diagrama Valido') : console.log(validate.errors)
